fea53e8186718f24be6a2b3eaf027a79cd1b86fa
Lex <info@alexandranel.com>
2023-12-21 19:27:23 +1100

    code is fully done, just running the clear function and local storage issues

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 var startGame = document.querySelector("#startGame"); //getting id for start button
 var timerDisplay = document.querySelector("#timer"); //getting id for countdown timer
-var secondsLeft = 76; //seconds left initialise the timer
+var secondsLeft = 50; //seconds left initialise the timer
 var mainEl = document.querySelector("#main"); //getting id for the body for quiz
 var quizContainer = document.querySelector("#quizContainer"); //container targeted for where the quiz goes
 var correctSound = new Audio("Assets/sound-correctAnswer.wav"); //loading a sound for correct answers
@@ -16,9 +16,12 @@ startGame.addEventListener("click", function (event) { //adds event listener to 
 });
 
 //TIMER FUNCTION BELOW
+
+var timerInterval; //setting this globally so I can stop the timer once our quiz is over within the quiz function also
+
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () { //setting our global variable to the set interval function
     secondsLeft--;
     timerDisplay.textContent = "Timer: " + secondsLeft;
 
@@ -107,83 +110,126 @@ var myQuestions = [
   }
 ]
 
-var currentQuestionIndex = 0; //creating an index for each question in the array so we 
-//can manage going through it one at a time
+var currentQuestionIndex = 0; //creating an index for each question in the array so we can manage going through it one at a time
 var keepScore = 0; //need a variable to tally the score of correct questions
+var userName = ""; //variable to store user name input id= playername
+var questionObj;
+
+localStorage.clear();
 
 //the function for the quiz itself
 function generateQuiz() {
-
+questionObj= myQuestions[currentQuestionIndex];   //question object is the specific indexed question within the myQuestionArray
   //clear the previous question/ area for new question
   quizContainer.innerHTML = "";
 
-  //generate the highscores input form when all questions are complete
+  //check if we are finished the quiz
   if (currentQuestionIndex >= myQuestions.length) {
-    window.location.href = "scores.html";
-  }
-  
-  //question object is the specific indexed question within the myQuestionArray
-  var questionObj = myQuestions[currentQuestionIndex];
+    clearInterval(timerInterval); //stop and clear the timer if we are finished the quiz
+
+    let playerForm = document.createElement("form"); //make the form container for the player to enter their details
+    let playerInput = document.createElement("input"); //make the input for the player to enter their details
+    let submitName = document.createElement("button"); //create a button to submit the name of the player
+
+    playerInput.setAttribute("id", "playerName"); //attaching an id to the input from player
+    playerInput.setAttribute("type", "text"); //setting the type of input to text
+    playerInput.setAttribute("placeholder", "Type your name here"); //making the input container have instructions as placeholder text
+
+    submitName.setAttribute("type", "submit"); //setting button type to submit
+    submitName.textContent = "submit"; //creating the text content of teh button to the instructional 'submit'
+
+    playerForm.appendChild(playerInput);
+    playerForm.appendChild(submitName);
+    quizContainer.appendChild(playerForm);
+
+    playerForm.addEventListener("submit", function (event) {
+      event.preventDefault();
+      if (playerInput.value.trim() === "") {
+        alert("please enter your name"); //if they enter nothing put an alert up 
+      } else {
+        userName = playerInput.value.trim(); //store user name input in my userName variable
+        userResults();// call my local storage function
+        window.location.href = "scores.html"; //redirect to high scores
+      }
+    });
+    
+  } else { //if we're not finished the quiz, go throught the quiz
   var questionElement = document.createElement("h2"); //generate an h1 for the question
   questionElement.textContent = questionObj.question; //grab the question string from the object
 
   var optionsList = document.createElement("ul"); //generate an h1 for the question
-
+  
   for (let key in questionObj.answers) { //using a for-in loop as my questions exist within an object
     let listItem = document.createElement("li");//createing a list item for each answer option and its radio button
-
     let radioButton = document.createElement("input"); //creating the radio buttons to select the correct answer to a question
+    let answerOption = document.createElement("label"); //this is the part of the ul that sits next to the radio button- the answer/option itself
+    
     radioButton.setAttribute("type", "radio"); //setting the type to radio- radio buttons can only have one selected
     radioButton.setAttribute("name", "question"); //setting the name of a group of these radio buttons to the question they are allocated to (ensuring only one can be chosen correctly in a question group)
     radioButton.setAttribute("value", key); //assigning a value to the radio button- being the key of the obj item /or answer option
-    radioButton.setAttribute("id", "option-" + key); //this creates a unique id for each radio button ie. "option-a" or "option-b" allowing me to target each uniquely
+    radioButton.setAttribute("id", "option-" + key); //this creates a unique id for each radio button ie. "option-a" or "option-b" allowing me to target each uniquely 
+    radioButton.setAttribute("class", "radioClass"); //creating a class
+    answerOption.setAttribute("for", "option-" + key); //"for" explicitly links the answer option to the radio button using the id that we previously created
+    answerOption.textContent = questionObj.answers[key]//setting the text to the array's object's property where I stored the correct answer
 
-    let answerOption = document.createElement("label"); //this is the part of the ul that sits next to the radio button- the answer/option itself
-    answerOption.setAttribute("for", "option-" + key) //"for" explicitly links the answer option to the radio button using the id that we previously created
-    answerOption.textContent = questionObj.answers[key]//setting the text to the array-object-object where I stored the asnwers 
-
-    let resultMessage = ""; //create a variable called results message- this string will eventually tell us if answer is correct or not
-    let messageEl=null; //we create an empty variable called messageEl, to populate with an html element later
-   
     listItem.appendChild(radioButton); //add the radio buttons to the html id tag
     listItem.appendChild(answerOption); //add the answer options to the html id tag
     optionsList.appendChild(listItem); //add the <ul> list items to the html id tag 
+ 
+   radioButton.addEventListener('change', chooseAnswer); //triggering an event function for teh radio button answer
 
-    let correctAnswer = questionObj.correctAnswer; //creating a correctAnswer variable to assign in our function
-
-    radioButton.addEventListener('change', function () { //triggering an event function for teh radio button answer
-
-      if (this.value === correctAnswer) { //if teh button that is pressed is the correct one
-        keepScore++; //add it to my score variable
-        correctSound.play(); //play this sound for correct answer
-        resultMessage = "Correct!" //this i the message that shows if the correct answer is chosen
-
-      } else if (secondsLeft > 0) { //if it is an incorrect answer and there are more than 0 seconds left     
-        secondsLeft--; //minus a second
-        timerDisplay.textContent = "Timer: " + secondsLeft; //then immediately deduct it from our timer
-        incorrectSound.play(); //and play the incorrectAnswer sound
-        resultMessage = "Wrong Answer! Correct answer is: " + questionObj.answers[correctAnswer]; //this is the message that is 
-        //shown if teh incorrect answer is chosen, we all add in what the correct answer would be
-      }
-
-      if (!messageEl) { //if the messageEl doesnt exist ie. is null (which we created before)
-        messageEl = document.createElement("cite"); //then create a html <cite> element for it
-        optionsList.appendChild(messageEl); //then add that to the options list 
-      }
-
-      messageEl.textContent=resultMessage; //now that our html is created in our if condition, and appended in our html, we assign the resultMessage to it
-      
-      setTimeout(function() { //this function exists within the event listeneing 
-        currentQuestionIndex++; //this function will go through the current question index (to next question)
-        generateQuiz(); //running through the generate quiz function
-      }, 2000); // after 2 seconds once teh event listener has been pressed
-    
-    });
+    quizContainer.appendChild(questionElement); //add the question to the html id tag 
+    quizContainer.appendChild(optionsList); //add the options for answers to the html id tag
   }
+}}
 
+//Keep Score 
+function userResults(){
+  var highScore = JSON.parse(localStorage.getItem("scores")) || []; //pull out existing score info from local storage unless theer are none, then its an array
+  var newScore= { userName, keepScore}; //create new score object
+  highScore.push(newScore); //put the new score info in the highscore with the others
 
-  quizContainer.appendChild(questionElement); //add the question to the html id tag 
-  quizContainer.appendChild(optionsList); //add the options for answers to the html id tag
+  localStorage.setItem("scores", JSON.stringify(highScore));
 }
 
-// localStorage.setItem()
+
+function chooseAnswer(){
+   questionObj= myQuestions[currentQuestionIndex];   //question object is the specific indexed question within the myQuestionArray again for this function
+    let correctAnswer = questionObj.correctAnswer; //creating a correctAnswer variable to assign in our function
+    let resultMessage = ""; //create a variable called results message- this string will eventually tell us if answer is correct or not
+    let messageEl = null; //// creating a null element for correct/incorrect answer message to trigge rinside teh event listener
+
+    var buttonSelections= document.querySelectorAll(".radioClass"); //calling the class of my radio buttons
+        buttonSelections.forEach(function(btn){
+      btn.disabled=true; //this function disables the other radio buttons once one is selected
+    })
+    
+
+      if (this.value === correctAnswer) { //if the button that is pressed is the correct one
+      keepScore++; //add it to my score variable
+      correctSound.play(); //play this sound for correct answer
+      resultMessage = "Correct!"; //this i the message that shows if the correct answer is chosen
+
+    } else if (secondsLeft > 0) { //if it is an incorrect answer and there are more than 0 seconds left     
+      secondsLeft--; //minus a second
+      timerDisplay.textContent = "Timer: " + secondsLeft; //then immediately deduct it from our timer
+      incorrectSound.play(); //and play the incorrectAnswer sound
+      resultMessage = "Wrong Answer! Correct answer is: " + questionObj.answers[correctAnswer]; //this is the message that is 
+      //shown if teh incorrect answer is chosen, we all add in what the correct answer would be
+    }
+
+    if (!messageEl){
+      messageEl=document.createElement("cite"); //create cite block for message feedback on answering question
+      quizContainer.appendChild(messageEl); //put the cite element underneath the question/answers list
+    }
+
+      messageEl.textContent = resultMessage; //connect the result message function to the message element to reveal it
+          
+      setTimeout(function () { //this function exists within the event listener 
+        buttonSelections.forEach(function(btn){
+        btn.disabled=false; //re-enables the radio buttons once we go to the next question
+      })
+      currentQuestionIndex++; //this function will go through the current question index (to next question)
+      generateQuiz(); //running through the generate quiz function
+    }, 1000); // after 2 seconds once teh event listener has been pressed
+  };
---
+++ scriptSCORES.js
@@ -0,0 +1,19 @@
+
+var highScore = JSON.parse(localStorage.getItem("scores")) || []; //getting any scores info from the localStorage
+
+highScore.sort(function(a,b){ //this sort function will make my high scores go from highest to lowest
+    return b.keepScore-a.keepScore //high scores function works on strings, so this establishes it as keepScore and makes it go from highest to lowest
+})
+
+for(i=0; i<highScore.length; i++){ //here we create the for loop to go through all of the high scores we have called in
+    var liEl = document.createElement("li"); //creating list elements for each highScore data 
+    liEl.textContent= highScore[i].userName + " - " + highScore[i].keepScore; //making the high scores data into text inside the list element
+
+    document.querySelector("ol").appendChild(liEl); //adding my list elements to my ordered list
+}
+
+var clearButton = document.getElementById("clear"); //grabbing the clear button from the html ifd for use
+
+clearButton.addEventListener("click", function() { //event listener for clearing the local storage on click
+    localStorage.clear();
+});
